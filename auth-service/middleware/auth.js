34308dea9a833fc48f1d7e82c173da570a1d28f4
tomamac <mek777@hotmail.co.th>
2025-05-15 22:19:44 +0700

    fix auth middleware

--- auth-service/middleware/auth.js
+++ auth-service/middleware/auth.js
@@ -10,7 +10,7 @@ auth = (req, res, next) => {
   const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(cookie, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.userId;
     next();
   } catch (error) {
