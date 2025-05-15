8e57b4653e6cb8dbc270ea88ee61014863ecb12e
tomamac <mek777@hotmail.co.th>
2025-05-15 18:29:56 +0700

    config cors to allow deployed frontend origin

--- auth-service/index.js
+++ auth-service/index.js
@@ -8,7 +8,12 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://taskit-4i9b.onrender.com/"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
