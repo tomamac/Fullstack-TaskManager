42774cbebe186e4d1568661f898e40baed84af5d
tomamac <mek777@hotmail.co.th>
2025-05-15 19:23:54 +0700

    fix cors in auth

--- auth-service/index.js
+++ auth-service/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://taskit-4i9b.onrender.com/"],
+    origin: ["http://localhost:5173", "https://taskit-4i9b.onrender.com"],
     credentials: true,
   })
 );
--- auth-service/routes/auth.js
+++ auth-service/routes/auth.js
@@ -47,6 +47,7 @@ router.post("/login", async (req, res) => {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         httpOnly: true,
         secure: true,
+        sameSite: "none",
       })
       .json({
         id: user._id,
